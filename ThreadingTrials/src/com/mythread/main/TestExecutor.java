f289607aa0f1ac170ea3a5c955753cecc309c6ed
Hendre <mahesh_hendre@nuance.com>
2021-08-20 20:53:05 +0530

    Own Executor implementation

---
+++ ThreadingTrials/src/com/mythread/main/TestExecutor.java
@@ -0,0 +1,25 @@
+package com.mythread.main;
+
+import com.mythread.FixedThreadExecutorService;
+import com.mythread.MyExecutorService;
+
+public class TestExecutor {
+
+	public static void main(String[] args) {
+		int executorSize = 5;
+		MyExecutorService executor = FixedThreadExecutorService.getFixedThreadExecutorService(executorSize);
+		
+		for (int i=0; i < executorSize*2; i++) {
+			executor.execute(()->{
+				try {
+					Thread.sleep(5000);
+					System.out.println("Finished task execution with: " + Thread.currentThread().getName());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			});
+		}
+		executor.shutDown();
+	}
+	
+}
